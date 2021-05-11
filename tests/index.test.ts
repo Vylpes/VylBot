6dc5172f994720274e116095177774a093b7633f
Ethan Lane <vylpes@outlook.com>
2021-05-11 20:59:01 +0100

    Fix actions

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,4 +1,4 @@
 // Blank test until we get others in, to test the functionality
-test('BLANK TEST PLEASE REMOVE', () => {
+test("BLANK TEST PLEASE REMOVE", () => {
     expect(true).toBeTruthy();
 });
